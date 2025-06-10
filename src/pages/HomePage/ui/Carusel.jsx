c06700ecbd26659c8b20eb7837b21b674f1af30b
EgrRas <egras8125@gmail.com>
2025-06-11 01:09:39 +0300

    щелфе

--- src/pages/HomePage/ui/Carusel.jsx
+++ src/pages/HomePage/ui/Carusel.jsx
@@ -1,10 +1,80 @@
-import React from 'react';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const clients = [
+    "photoes/main/womans/Woman1.png",
+    "photoes/main/womans/Woman2.png",
+    "photoes/main/womans/Woman3.png",
+    "photoes/main/womans/Woman4.png",
+    "photoes/main/womans/Woman5.png",
+];
 
 const Carusel = () => {
     return (
-        <div className="w-full flex justify-center mt-32">
-            <div className="w-[1000px]">
-                <p className="w-full text-[53px] font-unbounded font-light uppercase text-justify">Клиенты, которые уже работают по системе <span className="text-[#8296A6]">MNE IDET</span></p>
+        <div className="w-full flex justify-center mt-32 px-4">
+            <div className="w-full max-w-[1000px]">
+                <p className="text-[40px] lg:text-[53px] font-unbounded font-light uppercase text-[#1B3C4D] leading-tight">
+                    Клиенты, которые уже работают по системе{" "}
+                    <span className="text-[#8296A6] font-medium">MNE IDET</span>
+                </p>
+
+                <p className="text-[12px] uppercase pt-10">
+                    Нажмите на фото, чтобы посмотреть кейс
+                </p>
+
+
+
+                <div className="mt-10 relative pb-24">
+                    <Swiper
+                        modules={[Navigation, Pagination]}
+                        loop={true}
+                        spaceBetween={20}
+                        slidesPerView={4}
+                        navigation={{
+                            nextEl: ".swiper-next",
+                            prevEl: ".swiper-prev",
+                        }}
+                        pagination={{
+                            el: ".swiper-pagination",
+                            clickable: true,
+                            renderBullet: (index, className) => {
+                                return `<span class="${className} inline-block w-4 h-4 mx-1 rounded-full !bg-black transition-all duration-300"></span>`;
+                            },
+                        }}
+                        breakpoints={{
+                            320: { slidesPerView: 1.2 },
+                            640: { slidesPerView: 2 },
+                            1024: { slidesPerView: 3 },
+                            1280: { slidesPerView: 4 },
+                        }}
+                    >
+                        {clients.map((src, index) => (
+                            <SwiperSlide key={index}>
+                                <img
+                                    src={src}
+                                    alt={`client-${index}`}
+                                    className="rounded-3xl w-full h-[400px] object-cover"
+                                />
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+
+                    <div className="swiper-next absolute -right-20 top-[45%] -translate-y-1/2 z-10 w-16 h-16 flex items-center justify-center cursor-pointer bg-white">
+                        <img className="w-full" src="/photoes/main/NextButton.png" />
+                    </div>
+
+                    <div className="w-full flex justify-center">
+                        <div className="swiper-pagination absolute border-2 border-black bottom-2 left-0 w-full flex justify-center z-10" />
+                    </div>
+
+                    <p className="absolute top-[420px] left-10 text-[12px] underline cursor-pointer">
+                        Смотреть больше фото
+                    </p>
+                </div>
             </div>
         </div>
     );
