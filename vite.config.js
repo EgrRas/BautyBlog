e20e5829958a7b3039110adf01d7586278dd55de
Violent <Teymurkononov@yandex.ru>
2025-07-08 01:36:33 +0300

    Исправлен reload

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    allowedHosts: ["28f67dc2da04.ngrok-free.app", '127.0.0.1'],
+  }
 })
